4a7eca94d66066a5d445e175160a1e28cb9c8c2c
naerim <naerim1119@gmail.com>
2022-02-05 13:52:26 +0900

    stripe - 결제 시스템 구현

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ mongoose
   .then(() => console.log("DB Connection Successful!"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -22,6 +25,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
