8e1b7b359a68581014d5771e1ff374738162c70b
naerim <naerim1119@gmail.com>
2022-02-26 19:16:46 +0900

    fix stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
 
+// const KEY = process.env.STRIPE_KEY;
+// const stripe = require("stripe")(KEY);
+
+const Stripe = require("stripe");
+const stripe = Stripe(
+  "sk_test_51KPfeCBUYkIjKhCJYVsrT2cTEZlHGZOaG1GCIxTlLvOyIR5DL6inAbDZfUQDVMl0Q5ybca7ozO6yOhd3whkm2CPt00LzRXV18R"
+);
+
+console.log(process.env.STRIPE_KEY);
 router.post("/payment", (req, res) => {
   stripe.charges.create(
     {
